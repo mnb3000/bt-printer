5338053615a52a5fabbb24a784aaec3ef76248b5
mnb3000 <misha.mykolyshyn@gmail.com>
2025-06-06 23:09:25 +0300

    feat: wip changes, figuring out protocol

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+import EscPosEncoder from 'esc-pos-encoder';
+
+// @ts-ignore
+const encoder = new EscPosEncoder({
+  // @ts-ignore
+  printer_model: 'pos-5890',
+});
+
+const result = encoder
+  .initialize()
+  .text('The quick brown fox jumps over the lazy dog')
+  .newline()
+  .qrcode('https://nielsleenheer.com')
+  .encode();
+
+console.log(result);
